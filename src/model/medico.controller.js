6d4253e0fad98abc3a9a5e344b05899d6a7abe7d
leticiaSouzaDev <lec667121@gmail.com>
2024-08-13 17:06:52 -0300

    feat: adicionando o modelo Médico

---
+++ src/model/medico.controller.js
@@ -0,0 +1,33 @@
+import { DataTypes } from "sequelize";
+
+export const Medico = sequilize.define('Medico', {
+    MedicoId: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    Nome: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+
+    Email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+
+    Telefone: {
+        type: DataTypes.NUMBER,
+        allowNull: false,
+    },
+
+    Especialidade: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+
+    Hospital: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    }
+})
