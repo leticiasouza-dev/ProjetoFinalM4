7356ef93d28dd88fe180fdafc0777aefc763c19a
Clayton Bahia <gabriel808bahia@gmail.com>
2024-08-13 20:15:36 -0300

    Atualizando app js com rotas

--- app.js
+++ app.js
@@ -2,21 +2,25 @@ import express from 'express';
 import sequelize from '../ProjetoFinalM4/src/database/config.js';
 import corsConfig from './src/config/corsConfig.js';
 import { pacienteRouter } from '../src/routers/paciente.routes.js';
+import { agendamentoRouter } from './src/routers/agendamento.router.js';
+import { enfermeiroRouter } from './src/routers/enfermeiro.router.js'; 
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(corsConfig);
 
-app.use('/api', pacienteRouter)
+app.use('/api', pacienteRouter);
+app.use('/api', agendamentoRouter);
+app.use('/api', enfermeiroRouter); 
 
 sequelize
     .sync()
-    .then(()=>{
-        app.listen(PORT,()=>{
-			console.log(`A aplicação está rodando na porta http://localhost:${PORT}`);
-        })
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`A aplicação está rodando na porta http://localhost:${PORT}`);
+        });
     })
     .catch((erro) => {
-		console.error("Não foi possível conectar no banco de dados", erro);
-	});
+        console.error("Não foi possível conectar no banco de dados", erro);
+    });
