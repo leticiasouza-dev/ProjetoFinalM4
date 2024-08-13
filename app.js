b6f1d7e8fcef0cbfcd0dd3c07ee030b1450ddaae
Felipe Souza <fn23886@gmail.com>
2024-08-13 16:33:33 -0300

    feat: app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import { pacienteRouter } from './src/routers/paciente.routes.js';
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(express.json());
+
+app.use('/api', pacienteRouter)
+app.use('/api/voluntarios', voluntarioRouter); // erro ( não declarado! )
+
+app.listen(PORT,()=>{
+    console.log("Aplication ready")
+})
