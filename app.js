8553c98e5f47c7ed28011fc8102e6e0f7cb50d05
Felipe Souza <fn23886@gmail.com>
2024-08-13 17:26:49 -0300

    feat: cors and sqlite

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 import express from 'express';
+import sequelize from '../src/database/config.js';
+import corsConfig from './src/config/corsConfig.js';
 import { pacienteRouter } from '../src/routers/paciente.routes.js';
-
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
+app.use(corsConfig);
 
 app.use('/api', pacienteRouter)
 
-
-app.listen(PORT,()=>{
-    console.log("Aplication ready")
-})
+sequelize
+    .sync()
+    .then(()=>{
+        app.listen(PORT,()=>{
+			console.log(`A aplicação está rodando na porta http://localhost:${PORT}`);
+        })
+    })
+    .catch((erro) => {
+		console.error("Não foi possível conectar no banco de dados", erro);
+	});
